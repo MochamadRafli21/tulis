d9c94f47fd59d45b328e8d45675c382b381227d1
rafli <rafliforbusiness@gmail.com>
2024-03-27 00:18:02 +0700

    Mochamad Rafli | add additional column to migrations

--- src/libs/database/schema/blog/index.ts
+++ src/libs/database/schema/blog/index.ts
@@ -4,6 +4,9 @@ import { baseSchema } from '../base';
 export const blog = pgTable('blogs', {
   id: uuid('id').defaultRandom().primaryKey(),
   title: text('title').notNull(),
+  slug: text('slug').notNull().unique(),
+  banner: text('banner'),
+  subtitle: text('subtitle').default(''),
   content: text('content').notNull(),
   is_published: boolean('is_published').default(false),
   ...baseSchema,

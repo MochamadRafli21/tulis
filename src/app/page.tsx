8c8a49d5d7ddaf5f8f6f6560344293a92ca0736c
rafli <rafliforbusiness@gmail.com>
2024-03-30 01:26:29 +0700

    Mochamad Rafli | fix layouting in profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,30 +11,42 @@ import { SquarePen } from "lucide-react"
 export default async function Home() {
   const user = await getUser()
   const session = getSession()
+  if (user && !user?.banner) {
+    const placholderImage = await fetch('https://source.unsplash.com/random/?write')
+    user.banner = placholderImage.url
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center">
-      <div className={
-        user?.banner ? `bg-[url(${user?.banner}))]` : "bg-gradient-to-r from-cyan-500 to-blue-500 " + "w-full px-2 md:px-4 pt-4 flex flex-col place-items-center"}>
+      <div
+        style={{
+          backgroundImage: `url(${user?.banner})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+        className={"w-full px-2 md:px-4 pt-4 flex flex-col place-items-center"}>
 
-        <div className="w-full bg-white bg-opacity-95 backdrop-filter backdrop-blur-xl flex justify-between items-center rounded-lg border border-gray-300 shadow px-4 py-2 mb-4">
+        <div className="w-full bg-white bg-opacity-85 backdrop-filter backdrop-blur-xl flex justify-between items-center rounded-lg border border-gray-300 shadow px-4 py-2 mb-4">
           <h1 className="text-2xl font-semibold">BLOG</h1>
         </div>
 
 
         <div className="p-8 max-w-screen-lg mb-12 w-full justify-center md:justify-start lg:w-10/12">
-          <Card className="p-8 w-full md:w-[300px] bg-white bg-opacity-95 backdrop-filter backdrop-blur-xl ">
-            {
-              user?.avatar &&
-              <div className="relative w-full h-[300px] rounded-full">
-                <Image
-                  fill={true}
-                  className="w-full max-w-screen max-h-[300px] h-[300px] object-contain rounded-full"
-                  src={user?.avatar ?? ""}
-                  alt="banner"
-                />
-              </div>
-            }
+          <Card className="p-8 w-full md:w-[300px] bg-white bg-opacity-85 backdrop-filter backdrop-blur-xl ">
+            <div className="mb-4 flex justify-center">
+              {
+                user?.avatar &&
+                <div className="relative w-40 h-40 rounded-full">
+                  <Image
+                    fill={true}
+                    className="w-40 h-40 object-cover rounded-full"
+                    src={user?.avatar ?? ""}
+                    alt="banner"
+                  />
+                </div>
+              }
+            </div>
             <div className="flex justify-between items-center">
               <h1 className="text-2xl font-semibold col-span-2">{user?.name}</h1>
 

51d4f6b1208cc2fac83f321cffbaf54267888006
rafli <rafliforbusiness@gmail.com>
2024-06-17 22:00:04 +0700

    add suspence fallback

---
+++ src/app/(auth)/loading.tsx
@@ -0,0 +1,9 @@
+export default async function Loading() {
+  return (
+    <main className="flex min-h-screen flex-col items-center">
+      <div className="flex min-h-screen w-full px-4 py-3 md:py-0 justify-center items-center">
+        <h1 className="animate-bounce text-2xl font-semibold">Loading...</h1>
+      </div>
+    </main>
+  )
+}
